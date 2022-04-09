134e47e05903ed80d4bf802c05c77d47e6149ba7
Jeet Viramgama <jeet.viramgama@levaral.com>
2022-04-09 16:51:29 +0530

    added create api for todoz

--- api/todos.js
+++ api/todos.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { MongoClient } = require("mongodb");
 
-const uri = `mongodb+srv://jv20:${process.env.PASSWORD_MONGODB}@cluster0.ndfhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const uri = `mongodb+srv://jv20:${process.env.PASSWORD_MONGODB}@cluster0.ndfhw.mongodb.net/sample_todo?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 async function main() {
@@ -14,16 +17,48 @@ async function main() {
 main();
 
 const fetchAllTodos = async (res) => {
-  const result = await client.db("sample_todo").collection("todos").find();
-  console.log("result are", result);
-  //   res.json({
-  //     statu: 200,
-  //     message: "Fetched Successfully",
-  //     todos: result,
-  //   });
+  const result = await client
+    .db("sample_todo")
+    .collection("todos")
+    .find({})
+    .toArray();
+  // make sure to add .toArray() method for getting every document from the collection
+  if (result) {
+    res.json({
+      status: 200,
+      message: "Fetched Successfully",
+      todos: result,
+      success: true,
+    });
+  } else {
+    res.json({
+      status: 200,
+      message: "Fetched Successfully",
+      todos: result,
+      success: false,
+    });
+  }
 };
 
-// GET Todos List and return list | empty
+const insertTodo = async (todo, res) => {
+  const result = await client
+    .db("sample_todo")
+    .collection("todos")
+    .insertOne(todo);
+  if (result) {
+    res.json({
+      status: 200,
+      message: "Updated Successfully",
+      success: true,
+    });
+  } else {
+    res.json({
+      status: 200,
+      message: "Updated Successfully",
+      success: false,
+    });
+  }
+};
 
 router.get("/", async (req, res) => {
   //   console.log("req are", req.params);
@@ -32,6 +67,15 @@ router.get("/", async (req, res) => {
   } catch (error) {
     return res.status(500).send("Server Error");
   }
+});
+
+router.get("/add", async (req, res) => {
+  // console.log("req are", req.body);
+  try {
+    if (req.body) {
+      insertTodo(req.body, res);
+    }
+  } catch (error) {}
 });
 
 module.exports = router;
